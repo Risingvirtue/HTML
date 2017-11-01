128053883eb91a307c29abfdf749c83d296b542e
Johnny <risingvirtue@gmail.com>
2017-10-31 22:11:37 -0700

    enemies and classes

--- Shooter/shooterJS.js
+++ Shooter/shooterJS.js
@@ -13,17 +13,18 @@ var x = 0,
 	friction = 1;
 var mousePos = {x:0, y:0};
 var bullets = [];
+var enemies = [];
+var deadE = [];
 var mouseDown = false;
 var timeInterval = 300;
 var timeCounter = 0;
 let lastTime = 0;
 var ableToShoot = false;
 
+var FPS = new person(25,25,0,0,2);
 
-function enemy(x, y) {
-	
-}
-
+enemies.push(new enemy(100, 100, 25));
+enemies.push(new enemy(300, 100, 25));
 
 function update(time = 0) {
 	const deltaTime = time - lastTime;
@@ -33,95 +34,199 @@ function update(time = 0) {
 		timeCounter = 0;
 	}
 	lastTime = time;
-	
 	requestAnimationFrame(update);
 	
-	if (keys[0]) {
-		if (velX > -speed) {
-			velX--;
-		}
-	} else if (keys[2]) {
-		if (velX < speed) {
-			velX++;
-		}
-	} else {
-		velX = 0;
-	}
-	
-	if (keys[1]) {
-		if (velY > -speed) {
-			velY--;
-		}
-	} else if (keys[3]) {
-		if (velY < speed) {
-			velY++;
-		}
-	} else {
-		velY = 0;
-	}
-	
-	
-	velY *= friction;
-    y = (y + velY) % canvas.height;
-    velX *= friction;
-    x += velX;
-	if (x > (canvas.width) - 25) {
-		x -= velX;
-	} else if (x < 25){
-		x = 25;
-	}
-	if (y > (canvas.height) - 25) {
-		y -= velY;
-	} else if (y < 25){
-		y = 25;
-	}
 	
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
-	ctx.moveTo(x, y);
-    ctx.arc(x, y, 25, 0, Math.PI * 2);
-    ctx.fill();
-	ctx.moveTo(x, y);
-	let diffX = mousePos.x - x;
-	let diffY = mousePos.y - y;
-	let a = currAngle();
+	//body
+	FPS.movement();
+	FPS.draw();
 	
-	//console.log(angle *180/ Math.PI);
-	//console.log(Math.cos(angle), Math.sin(angle));
-	ctx.lineTo(50 * Math.cos(a) + x, 50 * Math.sin(a) + y);
-	ctx.lineWidth = 10;
-	ctx.stroke();
+	
+	//shooter
+	FPS.shooter();
+
+	
+	//angle 
+	var a = FPS.currAngle();
+	//bullets
 	if (mouseDown && ableToShoot) {
-		bullets.push({angle:a, x: 50 * Math.cos(a) + x, y: 50 * Math.sin(a) + y});
+		let temp = new bullet(a, 50 * Math.cos(a) + FPS.x, 50 * Math.sin(a) + FPS.y);
+		bullets.push(temp);
+		console.log(bullets);
 		ableToShoot = false;
 		timeCounter = 0;
-		}
+	}
 	var newBullets = [];
-	for (bullet of bullets) {
-		bullet.x += 10 * Math.cos(bullet.angle);
-		bullet.y += 10 * Math.sin(bullet.angle);
-		if (bullet.x > canvas.width || bullet.x < 0 || bullet.y > canvas.height || bullet.y < 0){
+	for (b of bullets) {
+		b.move();
+		if (b.x > canvas.width || b.x < 0 || b.y > canvas.height || b.y < 0){
 			continue;
 		} 
-		ctx.moveTo(bullet.x, bullet.y);
-		ctx.arc(bullet.x, bullet.y, 10, 0, Math.PI * 2);
-		ctx.fill();
-		newBullets.push(bullet);
+		b.draw();
+		newBullets.push(b);
 	};
 	bullets = newBullets;
+		
+	//enemies
+	var newEnemies = [];
+	for (e of enemies) {
+		let dead = false;
+		let index = 0;
+		for (b of bullets) {
+			let diffX = e.x - b.x;
+			let diffY = e.y - b.y;
+			if (Math.sqrt(diffX * diffX + diffY * diffY) < 50 + e.size) {
+				dead = true;
+				bullets.splice(index, 1);
+				break;
+			}
+			index++;
+		}
+		if (dead) {
+			deadE.push(e);
+			continue;
+		}
+		e.draw('#000000');
+		newEnemies.push(e);
+	}
+	enemies = newEnemies;
 	
+	newDead = [];
+	for (e of deadE) {
+		e.a *= 0.95;
+		e.draw('red');
+		if (e.a > 0.1) {
+			newDead.push(e);
+		}
+	}
+	deadE = newDead;
 }
 update();
 
-function currAngle() {
-	let diffX = mousePos.x - x;
-	let diffY = mousePos.y - y;
-	let angle = Math.atan(diffY/ diffX);
-	if (diffX < 0) {
-		angle += Math.PI;
+
+
+function person(x, y, velX, velY, speed) {
+	
+	this.x = x;
+	this.y = y;
+	this.velX = velX;
+	this.velY = velY;
+	this.speed = speed;
+	
+	this.movement = function () {
+		if (keys[0]) {
+			if (this.velX > -this.speed) {
+				this.velX--;
+			}
+		} else if (keys[2]) {
+			if (this.velX < this.speed) {
+			this.velX++;
+			}
+		} else {
+			this.velX = 0;
+		}
+		if (keys[1]) {
+			if (this.velY > -this.speed) {
+				this.velY--;
+			}
+		} else if (keys[3]) {
+			if (this.velY < this.speed) {
+				this.velY++;
+			}
+		} else {
+			this.velY = 0;
+		}
+		this.y += this.velY;
+		this.x += this.velX;
+		if (this.x > (canvas.width) - 25) {
+		this.x -= this.velX;
+		} else if (this.x < 25){
+			this.x = 25;
+		}
+		if (this.y > (canvas.height) - 25) {
+			this.y -= this.velY;
+		} else if (this.y < 25){
+			this.y = 25;
+		}
 	}
-	return angle;
+
+	this.draw = function () {
+		ctx.beginPath();
+		ctx.moveTo(this.x, this.y);
+		ctx.arc(this.x, this.y, 25, 0, Math.PI * 2);
+		ctx.fill();
+		ctx.closePath();
+	}
+	
+	this.shooter = function () {
+		ctx.beginPath();
+		ctx.moveTo(this.x, this.y);
+		let a = this.currAngle();
+		ctx.lineTo(50 * Math.cos(a) + this.x, 50 * Math.sin(a) + this.y);
+		ctx.lineWidth = 10;
+		ctx.stroke();
+	}
+	
+	this.currAngle = function() {
+		let diffX = mousePos.x - this.x;
+		let diffY = mousePos.y - this.y;
+		let angle = Math.atan(diffY/ diffX);
+		if (diffX < 0) {
+			angle += Math.PI;
+		}
+		return angle;
+	}
 }
+
+function bullet(angle, x , y) {
+	this.angle = angle;
+	this.x = x;
+	this.y = y;
+	
+	this.move = function() {
+		this.x += 10 * Math.cos(this.angle);
+		this.y += 10 * Math.sin(this.angle);
+	}
+	this.draw = function() {
+		ctx.moveTo(this.x, this.y);
+		ctx.arc(this.x, this.y, 10, 0, Math.PI * 2);
+		ctx.fill();
+	}
+}
+
+function enemy(x, y, size) {
+	this.x = x;
+	this.y = y;
+	this.size = size;
+	this.a = 1;
+	this.draw = function(color) {
+		ctx.beginPath();
+		ctx.fillStyle = color;
+		ctx.globalAlpha = this.a;
+		ctx.moveTo(this.x, this.y);
+		ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+		ctx.fill();
+		ctx.fillStyle = 'black';
+		ctx.globalAlpha = 1;
+	}
+	
+	
+}
+
+
+
+/*
+function drawBody() {
+	ctx.beginPath();
+	ctx.moveTo(x, y);
+    ctx.arc(x, y, 25, 0, Math.PI * 2);
+    ctx.fill();
+	ctx.closePath();
+}
+*/
+
 /*
 document.onkeydown = checkKey;
 
